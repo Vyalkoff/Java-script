14a9d378c9d2c54fabf546e106ffcf1a302485e9
vlad <kingbingping@gmail.com>
2021-09-06 17:03:53 +0500

    выполнил dz_6

--- dz_6/script.js
+++ dz_6/script.js
@@ -1,18 +1,22 @@
 const catalog = {
-    div_basket: document.querySelector('.basket'),
+    div_catalog: document.querySelector('.catalog'),
+
     product: [
         {
+            art: 1253,
             name: 'monitor',
             count: 10,
             price: 15000
         },
         {
+            art: 1896,
             name: 'laptop',
             count: 3,
             price: 30000
         },
 
         {
+            art: 234,
             name: 'printer',
             count: 4,
             price: 10000
@@ -25,40 +29,37 @@ const catalog = {
             <div><b>Цена</b>: ${good.price}</div>
             <div><b>Количество</b>: ${good.count}</div>
             <div><b>Стоимость</b>: ${good.count * good.price}</div><br>
-            <button>Купить</button><hr>
+            <button class='addBasketBtn' data-buy=${good.art}>Купить</button>
         </div>`;
     },
+    init() {
+        this.getCatalog();
+        this.addBtn()
 
-    getcatalog() {
+
+    },
+
+    getCatalog() {
 
         if (this.product.length) {
             this.product.forEach(element => {
-                this.div_basket.insertAdjacentHTML('beforeend', this.getBlock(element));
-                // this.div_basket.insertAdjacentHTML('beforeend', this.addBtn());
-
-            })
-            //     this.div_basket.insertAdjacentHTML('beforeend', `<p>В корзине: ${this.product.length} товара(ов) на сумму ${this.getTotal()} рублей</p>`);
-            // } else {
-            //     this.div_basket.insertAdjacentText('beforeend', '«Корзина пуста»');
-
-
+                this.div_catalog.insertAdjacentHTML('beforeend', this.getBlock(element));
+            });
         };
-
     },
-
     addBtn() {
 
 
 
+
+
     },
+
 
     getTotal() {
         return this.product.reduce((total, amount) => total += amount.price * amount.count, 0)
 
     },
-    clearBasket() {
-        this.product = []
-    }
 
 
 
@@ -67,11 +68,88 @@ const catalog = {
 };
 
 const basket = {
-    basketProduct: [],
+    div_basket: document.querySelector('.basket'),
 
+    basketProduct: [
+
+    ],
     init() {
 
-    }
+        this.render();
+        this.clearBasket();
+        this.addBasket();
+
+    },
+
+    render() {
+
+        if (this.getBasketLenght()) {
+            this.basketProduct.forEach(element => {
+                this.div_basket.insertAdjacentHTML('beforeend', this.getBasket(element));
+            });
+
+        } else {
+            this.div_basket.innerHTML = '';
+            this.div_basket.insertAdjacentHTML('beforeend', `В корзине ${this.getBasketLenght()} `)
+        }
+    },
+    getBasketLenght() {
+        return this.basketProduct.length
+
+    },
+
+    getBasket(product) {
+        return `<div class='basket_product'>
+        <h3>${product.name}</h3>
+        <div><b>Цена</b>: ${product.price}</div>
+        <div><b>Количество</b>: ${product.count}</div>
+        <div><b>Стоимость</b>: ${product.count * product.price}</div>
+        
+        </div>`;
+
+    },
+    clearBasket() {
+
+        let clr_btn = document.querySelector('.clr-basket')
+        clr_btn.addEventListener('click', (event) => {
+            this.basketProduct = [];
+            this.render();
+
+
+
+        });
+
+
+    },
+    addBasket() {
+        document.querySelector('.catalog').addEventListener('click', event => {
+            if (event.target.classList.contains('addBasketBtn')) {
+
+                let art = event.target.dataset.buy
+                let findProdCat = catalog.product.find(item => item.art == art);
+                let findproduct = this.basketProduct.find(item => item.art == art)
+
+                if (findproduct) {
+                    findproduct.count++
+
+
+                } else {
+                    this.basketProduct.push(Object.assign(findProdCat, { count: 1 }));
+
+
+                }
+                this.div_basket.innerHTML = ''
+                this.render();
+            }
+        });
+
+
+
+
+
+
+    },
+
 
 
 
@@ -80,5 +158,6 @@ const basket = {
 
 
 
-// basket.clearBasket();
-catalog.getcatalog();
+
+catalog.init();
+basket.init();
